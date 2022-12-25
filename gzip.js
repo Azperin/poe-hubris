68f4faac01fe9eb7bdae0ed35d4d078efe29aef3
Azperin <azperin@gmail.com>
2022-12-25 12:46:32 +0500

    auto

--- gzip.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const zlib = require('node:zlib');
-
-let a = fs.readFileSync('./htmlseedsinfo.js')
-fs.writeFileSync(`./htmlseedsinfo.js.gz`, zlib.gzipSync(a), { level: 9 });
